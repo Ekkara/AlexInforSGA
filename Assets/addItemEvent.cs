bb891c1886fa04e17deaeea1eac42a9fbf0acfad
Ekkara <47211128+Ekkara@users.noreply.github.com>
2019-07-13 17:13:26 +0200

    Rulator dialog samt idleAnimation fix

---
+++ Assets/addItemEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addItemEvent : MonoBehaviour {
+
+    public Item item;
+    [Tooltip("Removes the sprite from the map when picked up if 'true'")]
+    public bool removeOnPickup = false;
+    public AudioClip pickupClip;
+    
+    public void pickup()
+    {
+        if (pickupClip != null) AudioManager.instance.playSFXClip(pickupClip);
+        Debug.Log("Grabbed " + item.name);
+        Inventory.instance.AddItem(item);
+        if (removeOnPickup)
+        {
+            Destroy(this.gameObject);
+        }
+    }    
+}
